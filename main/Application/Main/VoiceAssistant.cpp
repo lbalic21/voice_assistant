51b213492bbcc7801bb7eca0b9e25e83489d651b
lbalic21 <luka.balic@fer.hr>
2024-01-19 18:16:14 +0100

    napokon

--- main/Application/Main/VoiceAssistant.cpp
+++ main/Application/Main/VoiceAssistant.cpp
@@ -6,10 +6,13 @@
 #include "AudioRecorder.hpp"
 #include "AudioESP.cpp"
 
+#include "../managed_components/espressif__esp-tflite-micro/tensorflow/lite/micro/micro_op_resolver.h"
+
 static const char *TAG = "VoiceAssistant";
 
 void Application()
 {
+    TfLiteStatus s;
     ESP_LOGI(TAG, "Starting the application.");
     AudioRecorder* recorder = new AudioESP();
     recorder->provide();
